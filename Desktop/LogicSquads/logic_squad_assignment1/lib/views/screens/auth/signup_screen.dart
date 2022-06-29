8fb4e7191c4d0760669a73335c075614aa429581
Asmat Wazir <71302616+Asmat906@users.noreply.github.com>
2022-06-29 18:53:08 +0500

    Update signup_screen.dart

--- Desktop/LogicSquads/logic_squad_assignment1/lib/views/screens/auth/signup_screen.dart
+++ Desktop/LogicSquads/logic_squad_assignment1/lib/views/screens/auth/signup_screen.dart
@@ -18,47 +18,10 @@ class SignupScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Text(
-            //   'Tiktok Clone',
-            //   style: TextStyle(
-            //     fontSize: 35,
-            //     color: buttonColor,
-            //     fontWeight: FontWeight.w900,
-            //   ),
-            // ),
-            // const Text(
-            //   'Register',
-            //   style: TextStyle(
-            //     fontSize: 25,
-            //     fontWeight: FontWeight.w700,
-            //   ),
-            // ),
+            
             const SizedBox(
               height: 25,
             ),
-            // Stack(
-            //   children: [
-            //     const CircleAvatar(
-            //       radius: 64,
-            //       backgroundImage: NetworkImage(
-            //           'https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png'),
-            //       backgroundColor: Colors.black,
-            //     ),
-            //     Positioned(
-            //       bottom: -10,
-            //       left: 80,
-            //       child: IconButton(
-            //         onPressed: () => authController.pickImage(),
-            //         icon: const Icon(
-            //           Icons.add_a_photo,
-            //         ),
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            // const SizedBox(
-            //   height: 15,
-            // ),
             Container(
               width: MediaQuery.of(context).size.width,
               margin: const EdgeInsets.symmetric(horizontal: 20),
